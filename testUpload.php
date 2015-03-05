69b328d374130aa446fbb957bd79c34a2c1dccbc
Max <chezshire@gmail.com>
2015-03-05 15:12:30 -0800

    WTF
    all i can say is what the fuck, github makes no sense to me

---
+++ testUpload.php
@@ -0,0 +1,33 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+  <title>GitHub Blows</title>
+  <meta name="description" content="The HTML5 Herald">
+  <meta name="author" content="SitePoint">
+
+  <link rel="stylesheet" href="css/styles.css?v=1.0">
+
+  <style>
+  	body {background-color: #ffc20f;}
+	.centered {
+	  position: fixed; /* or absolute */
+	  top: 50%;
+	  left: 50%;
+		}
+	</style>
+
+
+  <!--[if lt IE 9]>
+  <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+  <![endif]-->
+</head>
+
+<body>
+  <h1>so far...</h1>
+  <p>not a fan of gitHub</p>
+  <script src="js/scripts.js"></script>
+</body>
+</html>
